0ce79a5538bbd26ddd9a6211daf14e64c483714d
ian0011 <ianmoura607@gmail.com>
2024-10-28 20:10:01 -0300

    Criando Models Cadastro / migrate

--- cadastros/models.py
+++ cadastros/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+
+class Cadastro (models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=12)
+    email = models.EmailField(max_length=254)
+    created_date = models.DateTimeField(default=timezone.now)
+    notes = models.TextField(blank=True)
