5f7070f0ad324e58a7a2e73d7e7503d547996619
ian0011 <ianmoura607@gmail.com>
2025-03-16 20:12:37 -0300

    Ordenação, listagem e exibição página admin

--- cadastros/admin.py
+++ cadastros/admin.py
@@ -6,3 +6,8 @@ from cadastros import models
 class CadastroAdmin(admin.ModelAdmin):
     list_display = 'id','first_name', 'last_name', 'email', 'created_date',
     ordering = '-id',
+    list_filter = 'created_date',
+    search_fields = 'id', 'first_name', 'last_name',
+    list_per_page = 25
+    list_max_show_all = 200
+    list_display_links = 'id', 'first_name',
